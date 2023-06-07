894e633e10408cda10c50a460590565981fd5caa
RED <derekjones9817@gmail.com>
2023-06-08 04:12:29 +0900

    [6_8] bakcend fix

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/private', require('./routes/private'));
 app.use('/api/private/playGame', require('./routes/private'));
+app.use('/api/private/getUserData', require('./routes/private'));
+app.use('/api/private/updateBalance', require('./routes/private'));
 
 app.use(errorHandler);
 
